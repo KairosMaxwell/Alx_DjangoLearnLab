ccb1760963e8f223ccf5ac5a17b14d285c38a070
King Max <nyarkomaxwell03@gmail.com>
2025-03-12 14:09:41 -0600

    Urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+from .views import LibraryView ,list_all_books
 
 
+url_patterns = [
+    path("/relationship_app", list_all_books, name='list_all_books'),
+    path("/relationship_app", LibraryView, name='LibraryView')
 
-
-patterns=[
 
 ]
