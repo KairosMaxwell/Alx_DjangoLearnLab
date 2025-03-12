fe128201811399e71aefb4fcb2c7850e58e33926
King Max <nyarkomaxwell03@gmail.com>
2025-03-12 13:50:12 -0600

    View implementation

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Library,Book,Author
 
 # Create your views here.
