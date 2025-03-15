fb1e4c648cd765ead7f9d861c8850d701b27b378
King Max <nyarkomaxwell03@gmail.com>
2025-03-15 07:07:33 -0600

    CustomUser

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 admin.site.register(Library)
 
 admin.site.register(UserProfile)
+
+
+class CustomUserAdmin:
+    pass
+
+
+admin.site.register(CustomUser,CustomUserAdmin)
 
 
 
