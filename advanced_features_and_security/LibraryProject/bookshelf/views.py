83cf02b409a95fd021f0eb719a82428b306c4fdd
King Max <nyarkomaxwell03@gmail.com>
2025-03-15 07:52:30 -0600

    Managing Permissions in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
-@permission_required('app_name.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def view_instance(request, pk):
     # Logic for viewing an instance
     return render(request, 'view_instance.html')
 
-@permission_required('app_name.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_instance(request, pk):
     # Logic for editing an instance
     return render(request, 'edit_instance.html')
+
+
+def book_list(request):
+    """View to display a list of all books."""
+    books = Book.objects.all()  # Query all book instances from the database
+    return render(request, 'bookshelf/book_list.html', {'books': books})
