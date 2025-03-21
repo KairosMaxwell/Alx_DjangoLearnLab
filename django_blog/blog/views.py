985c1d9c12d3331649f4c875fbdc1bb56b251572
King Max <nyarkomaxwell03@gmail.com>
2025-03-21 16:35:19 -0600

    Templates implementation

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from rest_framework.reverse import reverse_lazy
+
 
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = ""
+
