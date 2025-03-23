21a43f773d1ea0658ba471e797dc74ed69ca78c4
King Max <nyarkomaxwell03@gmail.com>
2025-03-23 17:00:10 -0600

    Templates

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+'taggit',
     "blog.apps.BlogConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
 
 STATICFILES_DIRS = [BASE_DIR / "static/"]
 
