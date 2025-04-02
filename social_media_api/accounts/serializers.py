df4c7d84eca5bc526d951f50839ee580fa932c4a
King Max <nyarkomaxwell03@gmail.com>
2025-04-02 15:39:08 -0600

    User Created

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,29 @@
 from social_media_api.accounts.models import CustomUserModel
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+"""
+accounts/serializers.py doesn't contain: 
+["from rest_framework.authtoken.models import Token", 
+"serializers.CharField()",
+ "Token.objects.create", "get_user_model().objects.create_user"]
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    model = CustomUserModel
-    fields ="__all__"
+"""
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
 
     class Meta:
-        query_data = CustomUserModel.objects.all()
+        model = User
+        fields =["id","username","password","email","profile_picture"]
+
+    def createUser(self):
+        user =User.objects.create(**self.validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 
