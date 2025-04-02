01901c164cebca45820eff9e08195ad773d6ec55
King Max <nyarkomaxwell03@gmail.com>
2025-04-02 17:19:37 -0600

    Noble status

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,17 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
+from django.urls import include
 
 from . import views
 
 
 
-
-
-
-
-
 urlpattern =[
+    path("api",include('posts.urls')),
+
     path("register/",views.RegisterView.as_view(template_name=""),name="register"),
     path("login/",views.LoginView.as_view(template_name=""),name="login"),
     path("profile/",views.ProfileView,name="profile"),
