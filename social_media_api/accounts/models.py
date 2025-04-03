8e11fbfbe7f7eb55ba3a8f5942d1fafa08face43
King Max <nyarkomaxwell03@gmail.com>
2025-04-02 19:08:35 -0600

    Implementation Follow and Unfollow

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -9,7 +9,7 @@ User =get_user_model()
 
 
 
-class CustomUserModel(AbstractUser):
+class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to="profile_pictures", null=True, blank=True)
     following = models.ManyToManyField("self" ,symmetrical=False, related_name="following")
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,27 +1,27 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
-from social_media_api.accounts.models import CustomUserModel
+from social_media_api.accounts.models import CustomUser
 
 
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self,request,user_id):
-        who_to_follow = CustomUserModel.objects.get(id=user_id)
+        who_to_follow = CustomUser.objects.all()
         request.user.follow(who_to_follow)
-        return Response(f"You are following {who_to_follow.username} ")
+        return Response(f"You are following {who_to_follow} ")
 
 
 
 
-class UnFollowUserView(APIView):
+class UnFollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self,request,user_id):
-        who_to_unfollow = CustomUserModel.objects.get(id=user_id)
+        who_to_unfollow = CustomUser.objects.all()
         request.user.unfollow(who_to_unfollow)
-        return Response(f"message: You are unfollowed {who_to_unfollow.username} ")
+        return Response(f"message: You are unfollowed {who_to_unfollow} ")
 
 
 
